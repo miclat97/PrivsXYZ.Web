b4f044f0853a1a4eda765322a3e6b34ead4ae0ea
Michał Łatyński <michal@latynski.pl>
2023-06-03 21:22:47 +0200

    Migration contains Message table.

---
+++ PrivsXYZ.MVC/Migrations/20230603192216_MessageEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PrivsXYZ.MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Message = table.Column<string>(type: "longtext", nullable: true),
+                    Salt = table.Column<byte[]>(type: "longblob", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UploaderIPAddress = table.Column<string>(type: "longtext", nullable: true),
+                    UploaderHostname = table.Column<string>(type: "longtext", nullable: true),
+                    ViewerIPAddress = table.Column<string>(type: "longtext", nullable: true),
+                    ViewerHostname = table.Column<string>(type: "longtext", nullable: true),
+                    OpenDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+        }
+    }
+}
