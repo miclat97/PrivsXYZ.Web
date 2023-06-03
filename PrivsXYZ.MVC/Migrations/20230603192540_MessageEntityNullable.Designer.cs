824cf0e4128f9fa31798dd0235cfc1fea4abec6c
Michał Łatyński <michal@latynski.pl>
2023-06-03 21:26:04 +0200

    Nullable opions: CreteDate and OpenDate in Messages.

--- PrivsXYZ.MVC/Migrations/20230603192540_MessageEntityNullable.Designer.cs
+++ PrivsXYZ.MVC/Migrations/20230603192540_MessageEntityNullable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrivsXYZ.MVC.Database;
 
@@ -10,9 +11,11 @@ using PrivsXYZ.MVC.Database;
 namespace PrivsXYZ.MVC.Migrations
 {
     [DbContext(typeof(PrivsXYZDbContext))]
-    partial class PrivsXYZDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230603192540_MessageEntityNullable")]
+    partial class MessageEntityNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
