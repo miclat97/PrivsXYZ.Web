c3cd62e032135d2401b0c8660f1e331d2d614ef3
miclat97 <michal.latynski@outlook.com>
2024-08-26 12:49:54 +0200

    Upgrade to .NET 8 and fix warning in Program.cs getting connectionstring.

--- PrivsXYZ.MVC/Program.cs
+++ PrivsXYZ.MVC/Program.cs
@@ -14,7 +14,7 @@ namespace PrivsXYZ.MVC
             builder.Services.AddControllersWithViews();
 
             builder.Services.AddDbContext<PrivsXYZDbContext>(options =>
-                options.UseMySQL(builder.Configuration.GetConnectionString("Database")));
+                options.UseMySQL(builder.Configuration.GetConnectionString("Database")!));
 
             //Register services
             builder.Services.AddTransient<IMessageService, MessageService>();
